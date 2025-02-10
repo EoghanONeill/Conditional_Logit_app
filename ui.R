829e5940ea9fb8eabe553bc45af87175446afb79
EoghanONeill <oneill.eoghan.1@gmail.com>
2025-02-10 22:17:18 +0100

    edited title

--- ui.R
+++ ui.R
@@ -1,5 +1,5 @@
 shinyUI(pageWithSidebar(
-  headerPanel('Multinomial Logistic Regression Probabilities'),
+  headerPanel('Conditional Logistic Regression Probabilities'),
   sidebarPanel(
     sliderInput('b0_j1', 'Select intercept parameter for j=1', value = 1, min = -10, max = 10, step = 0.25),
     sliderInput('b0_j2', 'Select intercept parameter for j=2', value = -1, min = -10, max = 10, step = 0.25),
